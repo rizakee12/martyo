bbf2319cb2f3bcfc5b11ebb4f0f110e2e5e59d08
rizakee12 <leomangubat42@gmail.com>
2024-07-12 14:24:44 +0530

    first React Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div></div>
+      <h1>Francis Vincent M Martinez</h1>
+      <h2>Leo Nino Lariego </h2>
+    </div>
+  );
+}
+
+export default App;
